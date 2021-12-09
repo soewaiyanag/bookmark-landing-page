e7b2dd96120feabc040b4d0cc2b1ab344840021d
soewaiyanag <soewaiyanag@gmail.com>
2021-12-09 17:10:43 +0630

    add feature section with selector

--- script.js
+++ script.js
@@ -1,22 +1,42 @@
-const hamburger = document.querySelector(".hamburger");
-const nav = document.querySelector("nav");
-const animateDiv = document.querySelector(".animate");
+// MOBILE NAV TOGGLE
+(function () {
+  const hamburger = document.querySelector(".hamburger");
+  const nav = document.querySelector("nav");
 
-hamburger.addEventListener("click", () => {
-  nav.classList.toggle("is-active");
-});
+  hamburger.addEventListener("click", () => {
+    nav.classList.toggle("is-active");
+  });
+})();
 
-const callback = (entries, observer) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add("animated");
-      observer.disconnect();
-    }
+// SCROLL ANIMATION
+(function () {
+  const animateDiv = document.querySelector(".animate");
+  const callback = (entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("animated");
+        observer.disconnect();
+      }
+    });
+  };
+
+  const myObserver = new IntersectionObserver(callback, {
+    threshold: 0.5,
+  });
+
+  myObserver.observe(animateDiv);
+})();
+
+// FEATURE SELECTOR
+const featureSelectors = document.querySelectorAll(".select-feature");
+const resetSelectors = () => {
+  featureSelectors.forEach((selector) => {
+    selector.dataset.checked = false;
   });
 };
-
-const myObserver = new IntersectionObserver(callback, {
-  threshold: 0.5,
+featureSelectors.forEach((selector) => {
+  selector.addEventListener("click", () => {
+    resetSelectors();
+    selector.dataset.checked = true;
+  });
 });
-
-myObserver.observe(animateDiv);
