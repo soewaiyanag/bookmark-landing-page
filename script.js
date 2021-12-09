3849b0ddb7cee11eb73b950269b84cdaa0ff468e
soewaiyanag <soewaiyanag@gmail.com>
2021-12-09 13:51:20 +0630

    added animation on scroll with pure javascript

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 const hamburger = document.querySelector(".hamburger");
 const nav = document.querySelector("nav");
+const animateDiv = document.querySelector(".animate");
 
 hamburger.addEventListener("click", () => {
   nav.classList.toggle("is-active");
 });
 
-AOS.init();
+const callback = (entries, observer) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("animated");
+      observer.disconnect();
+    }
+  });
+};
+
+const myObserver = new IntersectionObserver(callback, {
+  threshold: 0.5,
+});
+
+myObserver.observe(animateDiv);
